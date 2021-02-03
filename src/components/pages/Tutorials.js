ab16eeea58786f4421a2f1eb876c6331354b8ec7
unknown <donovantthacker@gmail.com>
2021-02-03 12:21:07 -0500

    Updated Tutorial Page and Navbar Padding one more time

--- src/components/pages/Tutorials.js
+++ src/components/pages/Tutorials.js
@@ -3,10 +3,30 @@ import { Link } from 'react-router-dom';
 
 export default function Tutorials(){
     return(
-        <div className="tutorials">
-            <h1 >
-                <Link to="/TutorialList" className="tutorial-link"> TUTORIALS </Link>
-            </h1>
+        <div>
+            <div className="tutorials">
+                <h1 >
+                    <Link to="/TutorialList" className="tutorial-link"> TUTORIALS </Link>
+
+                    
+                </h1>
+            </div>
+            <div className="Page Padded">
+                <h1 className="Centered"> TUTORIALS </h1>
+
+                <br/> 
+
+                <p> I find that I learn best by taking notes. I also find that I frequently revisit my notes when programming because I work like a machine but don't have the memory of one. </p>
+
+                <br/> 
+
+                <p> I also want to build a respectable portfolio to demonstrate that I am a capable front-end developer. </p>
+
+                <br/> 
+
+                <p> And finally, I want to work on more projects that have the potential to earn me passive income. </p>
+
+            </div>
         </div>
     )
 }
