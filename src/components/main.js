c1741e2228d714daec201184118256f3acaf0912
unknown <donovantthacker@gmail.com>
2021-02-03 10:58:44 -0500

    Fixed import in main, we'll see if it helps

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Navbar from 'Navbar';
+import Navbar from './Navbar';
 import Home from './home';
 import Platform from './platform';
 import Welcome from './welcome';
